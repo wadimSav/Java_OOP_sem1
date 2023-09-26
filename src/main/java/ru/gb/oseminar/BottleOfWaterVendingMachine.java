fd9ac3de4827b16741ff7a71f5f7c47fa5b7710d
killkuruzzza <46843343+killkuruzzza@users.noreply.github.com>
2023-09-26 10:36:07 +0300

    Update BottleOfWaterVendingMachine.java

--- src/main/java/ru/gb/oseminar/BottleOfWaterVendingMachine.java
+++ src/main/java/ru/gb/oseminar/BottleOfWaterVendingMachine.java
@@ -1,32 +1,41 @@
 package ru.gb.oseminar;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class BottleOfWaterVendingMachine implements VendingMachine {
-    private final List<Product> products;
+public class BottleOfWaterVendingMachine implements VendingMachine{
+     List<BottleOfWater> productList;
 
-    public BottleOfWaterVendingMachine(List<Product> products) {
-        this.products = products;
+    public List<BottleOfWater> getProductList() {
+        return productList;
     }
 
-    public Product getProduct(String name){
-        for(Product product: products){
-            if(product.getName().equalsIgnoreCase(name)){
+    public void setProductList(List<BottleOfWater> productList) {
+        this.productList = new ArrayList<>(productList);
+    }
+
+    public BottleOfWaterVendingMachine(List<BottleOfWater> productList) {
+        this.productList = productList;
+    }
+    public BottleOfWater getProduct(String name, int volume) {
+        for (BottleOfWater bottleofWater : productList) {
+            if (bottleofWater.getName().equals(name) && bottleofWater.getVolume() == volume) {
+                return bottleofWater;
+            }
+        }
+        return null;
+    }
+    @Override
+    public Product getProduct(String name) {
+        for (Product product : productList){
+            if(product.getName().equals(name)){
                 return product;
             }
         }
-        throw new IllegalStateException(String.format("Продукт c названием %s не найден.", name));
+        return null;
     }
 
-    public BottleOfWater getProduct(String name, int volume){
-        for(Product product: products){
-            if(product instanceof BottleOfWater){
-                if(product.getName().equalsIgnoreCase(name) && ((BottleOfWater) product).getVolume() == volume){
-                    return (BottleOfWater) product;
-                }
-            }
-
-        }
-        throw new IllegalStateException(String.format("Продукт c названием %s не найден.", name));
+    public void addBottleOfWater(BottleOfWater bottleofWater){
+        this.productList.add(bottleofWater);
     }
 }
